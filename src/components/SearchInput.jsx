b0faa91d975027b670bd056e690222776b3f4d14
Lucia Brammer <brammerl96@gmail.com>
2025-05-09 14:21:45 -0700

    feat: Create search input

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,8 @@
+const SearchInput = ({value, onChange, placeholder}) => {
+
+
+
+    return (<input type='text' onChange={(e) => { console.log(e); onChange(e)}} value={value} placeholder={placeholder}/>)
+}
+
+export default SearchInput;
