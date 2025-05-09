2062faca99317a6513a3a9cb5b13c8087c97b4f2
Lucia Brammer <brammerl96@gmail.com>
2025-05-08 20:01:28 -0700

    chore: Fetch movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import MovieList from './components/MovieList.jsx'
+import MovieCard from './components/MovieCard.jsx'
+
+import useFetchData from './hooks/useGetNowPlaying.jsx'
+import { REQUEST_METHOD } from './constants/constants.js'
+
 
 const App = () => {
-  <div className="App">
-    
-  </div>
+  const [movies, setMovies] = useState([])
+
+  const {data, error, loading } = useFetchData(REQUEST_METHOD.GET, import.meta.env.VITE_NOW_PLAYING_URL)
+
+
+  if (loading) {
+    return <div>loading</div>
+  }
+
+  return (  
+    <MovieList>
+      {
+        data.results.map((movie) => <MovieCard movie={movie}/>)
+      }
+    </MovieList>
+  )
+
 }
 
 export default App
