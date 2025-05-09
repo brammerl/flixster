16ab1eeff4341abbc79e593e79d0d24e92c2708c
Lucia Brammer <brammerl96@gmail.com>
2025-05-08 20:02:43 -0700

    chore: Temp movie card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,14 @@
+const MovieCard = ({movie}) => {
+    
+    return (
+    <article key={1}>
+        <div class='movie-image'>
+            <img src={"/temp"}/>
+        </div>
+        <div class='movie-info'>
+        hello
+        </div>
+    </article>)
+}
+
+export default MovieCard;
