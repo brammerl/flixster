2549e06957838523961fe8dbe7b5f41735692eb1
Lucia Brammer <brammerl96@gmail.com>
2025-05-09 14:22:23 -0700

    chore: Create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-const Header = () => {
-    <header>
+import SearchInput from "./SearchInput";
 
+const Header = ({children}) => {
+
+    return(   
+     <header className='App-header'>
+        {children}
     </header>
-}
+)}
 
 export default Header;
