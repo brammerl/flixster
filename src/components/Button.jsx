1e126fd764f54501e5c13b176e31d19a203c4d97
Lucia Brammer <brammerl96@gmail.com>
2025-05-09 14:22:11 -0700

    chore: Update button component to have text

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,14 +7,14 @@ const BUTTON_SIZES = {
     default: 'default-button'
 }
 
-const Button = ({onClick, size='default', color='primary'}) => {
+const Button = ({onClick, size='default', color='primary', text='Click me!'}) => {
     const sizeClassName = `${BUTTON_SIZES[size]}`
     const colorClassName = `${BUTTON_COLORS[color]}`
 
     const finalClassName = `${sizeClassName} ${colorClassName}`
     
 
-    return (<button className={finalClassName} onClick={() => onClick()}>CLICK ME</button>)
+    return (<button className={finalClassName} onClick={() => onClick()}>{text}</button>)
 }
 
 export default Button;
