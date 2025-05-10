7d58e780f9ca23650f0c65707495ecabd16d50fe
Lucia Brammer <brammerl96@gmail.com>
2025-05-09 19:57:55 -0700

    chore: Update onChange

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -2,7 +2,7 @@ const SearchInput = ({ value, onChange, placeholder }) => {
   return (
     <input
       type="text"
-      onChange={(e) => onChange(e)}
+      onChange={(e) => onChange(e.target.value)}
       value={value}
       placeholder={placeholder}
     />
